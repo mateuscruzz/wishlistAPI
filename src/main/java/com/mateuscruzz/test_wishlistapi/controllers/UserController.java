f8e0732f850d679ee976b0b57e034e174f3f2925
mateus <mateus.cruz14m@gmail.com>
2024-09-21 20:05:56 -0300

    user controller, createUser method

---
+++ src/main/java/com/mateuscruzz/test_wishlistapi/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.mateuscruzz.test_wishlistapi.controllers;
+
+import com.mateuscruzz.test_wishlistapi.entity.User;
+import com.mateuscruzz.test_wishlistapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User createdUser = userService.createUser(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        }
+        catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+}
--- src/main/java/com/mateuscruzz/test_wishlistapi/services/UserService.java
+++ src/main/java/com/mateuscruzz/test_wishlistapi/services/UserService.java
@@ -7,6 +7,8 @@ import com.mateuscruzz.test_wishlistapi.repositories.WishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -17,6 +19,17 @@ public class UserService {
     private WishlistRepository wishlistRepository;
 
     public User createUser(User user) {
+        Optional<User> existingUserByEmail = userRepository.findByEmail(user.getEmail());
+        Optional<User> existingUserByPassword = userRepository.findByPassword(user.getPassword());
+
+        if (existingUserByEmail.isPresent()) {
+            throw new IllegalArgumentException("User with this email already exists");
+        }
+
+        if (existingUserByPassword.isPresent()) {
+            throw new IllegalArgumentException("User with this password already exists");
+        }
+
         User savedUser = userRepository.save(user);
         Wishlist wishlist = new Wishlist();
         wishlist.setUser(savedUser);

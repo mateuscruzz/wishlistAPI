5c977e896f1f608ac6770830a2fc34c022ee6782
mateus <mateus.cruz14m@gmail.com>
2024-09-21 20:05:56 -0300

    user controller, createUser method

--- src/main/java/com/mateuscruzz/test_wishlistapi/repositories/UserRepository.java
+++ src/main/java/com/mateuscruzz/test_wishlistapi/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import com.mateuscruzz.test_wishlistapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPassword(String password);
     User findByUsername(String username);
-    User findByEmail(String email);
 }

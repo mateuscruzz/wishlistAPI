64bc27f127dd61a7291b31914f421d4b7ab82aea
mateus <mateus.cruz14m@gmail.com>
2024-09-22 22:34:55 -0300

    game test model

---
+++ src/main/java/com/mateuscruzz/test_wishlistapi/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.mateuscruzz.test_wishlistapi.repositories;
+
+import com.mateuscruzz.test_wishlistapi.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, UUID> {
+}

5767954c26a342db3bfd54050569cd0b934814ac
mateus <mateus.cruz14m@gmail.com>
2024-09-24 14:16:52 -0300

    get all users

--- src/main/java/com/mateuscruzz/test_wishlistapi/services/UserService.java
+++ src/main/java/com/mateuscruzz/test_wishlistapi/services/UserService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -34,6 +35,10 @@ public class UserService {
 
     public Optional<User> getUserById(UUID id) {
         return userRepository.findById(id);
+    }
+
+    public Optional<List<User>> getAllUsers() {
+        return Optional.of(userRepository.findAll());
     }
 
     public Optional<Void> deleteUserById(UUID id) {
